1bbff030d00138059799ec405821d1cfbb52006a
Fabio “SolidGold” Paduanella <fabio.paduanella@truufles.com>
2025-08-05 17:44:41 +0100

    Added function buttonPressed to take action based on the button pressed.

--- javascript.js
+++ javascript.js
@@ -4,129 +4,142 @@ const container = document.querySelector("#container");
 const result = document.querySelector("#result");
 
 function createGrid() {
-
-    // for (i = 1; i < rowsNum + 1; i++) {
-        let i = 9;
-        while (i >= 0) {
-            
-            if (i === 6) {
-                const buttondiv = document.createElement("button");
-                buttondiv.classList.toggle(`number`);
-                buttondiv.classList.toggle(`bordered`);
-                buttondiv.classList.toggle(`background`);
-                buttondiv.textContent = "/";
-                // button.style.flexBasis = "10vh";
-                buttondiv.addEventListener('click', () => {
-                    result.textContent = buttondiv.textContent;
-                });
-            
-                container.appendChild(buttondiv);
-            } else if (i === 3) {
-                const button = document.createElement("button");
-                button.classList.toggle(`number`);
-                button.classList.toggle(`bordered`);
-                button.classList.toggle(`background`);
-                button.textContent = "*";
-                // button.style.flexBasis = "10vh";
-                button.addEventListener('click', () => {
-                    result.textContent = button.textContent;
-                });
-            
-                container.appendChild(button);
-            // }
-            } else if (i === 0) {
-
-                const buttonplus = document.createElement("button");
-                buttonplus.classList.toggle(`number`);
-                buttonplus.classList.toggle(`bordered`);
-                buttonplus.classList.toggle(`background`);
-                buttonplus.textContent = "+";
-                // button.style.flexBasis = "10vh";
-                buttonplus.addEventListener('click', () => {
-                    result.textContent = buttonplus.textContent;
-                });
-                container.appendChild(buttonplus)
-
-                const buttondec = document.createElement("button");
-                buttondec.classList.toggle(`number`);
-                buttondec.classList.toggle(`bordered`);
-                buttondec.classList.toggle(`background`);
-                buttondec.textContent = ".";
-                // button.style.flexBasis = "10vh";
-                buttondec.addEventListener('click', () => {
-                    result.textContent = buttondec.textContent;
-                });
-            
-                container.appendChild(buttondec);
-                
-            }
+    let i = 9;
+    while (i >= 0) {
+        
+        if (i === 6) {
+            const buttondiv = document.createElement("button");
+            buttondiv.classList.toggle(`number`);
+            buttondiv.classList.toggle(`bordered`);
+            buttondiv.classList.toggle(`background`);
+            buttondiv.textContent = "/";
+            // button.style.flexBasis = "10vh";
+            buttondiv.addEventListener('click', () => {
+                result.textContent = buttonPressed(buttondiv.textContent);
+            });
+        
+            container.appendChild(buttondiv);
+        } else if (i === 3) {
             const button = document.createElement("button");
             button.classList.toggle(`number`);
             button.classList.toggle(`bordered`);
             button.classList.toggle(`background`);
-            // innerDiv.setAttribute("id", `divR${i}C${j}`);
-            // let size = 5;
+            button.textContent = "*";
             // button.style.flexBasis = "10vh";
-            button.textContent = i;
-            
             button.addEventListener('click', () => {
-                result.textContent = button.textContent;
+                result.textContent = buttonPressed(button.textContent);
             });
+        
             container.appendChild(button);
+        } else if (i === 0) {
 
-            if (i === 0) {
-                const buttoneq = document.createElement("button");
-                buttoneq.classList.toggle(`number`);
-                buttoneq.classList.toggle(`bordered`);
-                buttoneq.classList.toggle(`background`);
-                buttoneq.textContent = "=";
-                // button.style.flexBasis = "10vh";
-                buttoneq.addEventListener('click', () => {
-                    result.textContent = buttoneq.textContent;
-                });
-                container.appendChild(buttoneq);
+            const buttonplus = document.createElement("button");
+            buttonplus.classList.toggle(`number`);
+            buttonplus.classList.toggle(`bordered`);
+            buttonplus.classList.toggle(`background`);
+            buttonplus.textContent = "+";
+            // button.style.flexBasis = "10vh";
+            buttonplus.addEventListener('click', () => {
+                result.textContent = buttonPressed(buttonplus.textContent);
+            });
+            container.appendChild(buttonplus)
 
-                const buttonminus = document.createElement("button");
-                buttonminus.classList.toggle(`number`);
-                buttonminus.classList.toggle(`bordered`);
-                buttonminus.classList.toggle(`background`);
-                buttonminus.textContent = "-";
-                // button.style.flexBasis = "10vh";
-                buttonminus.addEventListener('click', () => {
-                    result.textContent = buttonminus.textContent;
-                });
+            const buttondec = document.createElement("button");
+            buttondec.classList.toggle(`number`);
+            buttondec.classList.toggle(`bordered`);
+            buttondec.classList.toggle(`background`);
+            buttondec.textContent = ".";
+            // button.style.flexBasis = "10vh";
+            buttondec.addEventListener('click', () => {
+                result.textContent = buttonPressed(buttondec.textContent);
+            });
+            container.appendChild(buttondec);
             
-                container.appendChild(buttonminus);
-            }
-            // const innerDiv = document.createElement("div");
-            i--;
-            // innerDiv.classList.toggle(`innerDiv`);
-            // innerDiv.classList.toggle(`row${i}`);
-            // innerDiv.classList.toggle(`col${j}`);
-            // innerDiv.setAttribute("id", `divR${i}C${j}`);
-            // let size = 100 / rowsColsNum;
-            // innerDiv.style.flexBasis = size + "%";
-            // innerDiv.addEventListener('mouseenter', () => {
-            //     innerDiv.classList.add('hoveredDiv');
-            // });
-            // container.appendChild(innerDiv);
-        } 
+        }
+        const button = document.createElement("button");
+        button.classList.toggle(`number`);
+        button.classList.toggle(`bordered`);
+        button.classList.toggle(`background`);
+        button.textContent = i;  
+        button.addEventListener('click', () => {
+            result.textContent = buttonPressed(button.textContent);
+        });
+        container.appendChild(button);
 
+        if (i === 0) {
+            const buttoneq = document.createElement("button");
+            buttoneq.classList.toggle(`number`);
+            buttoneq.classList.toggle(`bordered`);
+            buttoneq.classList.toggle(`background`);
+            buttoneq.textContent = "=";
+            // button.style.flexBasis = "10vh";
+            buttoneq.addEventListener('click', () => {
+                result.textContent = buttonPressed(buttoneq.textContent);
+            });
+            container.appendChild(buttoneq);
 
-    // }
+            const buttonminus = document.createElement("button");
+            buttonminus.classList.toggle(`number`);
+            buttonminus.classList.toggle(`bordered`);
+            buttonminus.classList.toggle(`background`);
+            buttonminus.textContent = "-";
+            // button.style.flexBasis = "10vh";
+            buttonminus.addEventListener('click', () => {
+                result.textContent = buttonPressed(buttonminus.textContent);
+            });
+            container.appendChild(buttonminus);
+        }
+    } 
 }
 
 createGrid();
 
-let number1;
-let operator;
-let number2;
+let number1 = NaN;
+let operator = "";
+let number2 = NaN;
 
 const clear = document.querySelector("#clear");
     // button.style.flexBasis = "10vh";
     clear.addEventListener('click', () => {
         result.textContent = "0";
+        number1 = NaN;
+        operator = "";
+        number2 = NaN;
     });
+
+function numberPressed(number) {
+    if (number == ".") {
+        if 
+        return 
+    }
+}
+
+
+function buttonPressed(button) {
+    switch (button) {
+        case 0:
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+        case 6:
+        case 7:
+        case 8:
+        case 9:
+        case ".":
+            return numberPressed(button);
+            break;
+        case "/":
+        case "*":
+        case "-":
+        case "+":
+        case "=":
+            return operatorPressed(button);
+            break;
+        default: return "ERROR buttonPressed"
+    }
+}
 
 function operate(operator, a, b) {
     switch (operator) {
@@ -137,7 +150,7 @@ function operate(operator, a, b) {
         case "/": return divide(a, b);
         break;
         case "*": return multiply(a, b);
-        default: return "ERROR";
+        default: return "ERROR operate";
     }
 }
 
